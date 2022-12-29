806e13b66ab343694a8723708aeb46f9bbc4996c
Tommaso Guidi <S0R3d@users.noreply.github.com>
2022-12-29 17:16:41 +0100

    Add basics PHP to comunicate with SQL db

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    require 'php/conn.php';
+    require 'php/table.php';
+    
+    echo '0';
+?>
---
+++ php/table.php
@@ -0,0 +1,13 @@
+<?php
+    $table = 'test';
+    try {
+        $db->exec('CREATE TABLE IF NOT EXISTS test(
+            id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+            name VARCHAR(255),
+            PRIMARY KEY(id)
+            )');
+    } catch (PDOException $th) {
+        echo 'Error massage: '.$th;
+        die();
+    }
+?>
