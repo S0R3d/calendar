81b1a4a4f419a6d4331b071c55f8fcb9edbefa44
Tommaso Guidi <S0R3d@users.noreply.github.com>
2022-12-29 19:32:18 +0100

    Changes in 'events' schema

---
+++ sql/create_db.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS calendar;
+
+CREATE DATABASE IF NOT EXISTS calendar;
+USE calendar;
+
+DROP TABLE IF EXISTS `events`;
+
+CREATE TABLE IF NOT EXISTS `events`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `start` DATETIME NOT NULL,
+    `finish` DATETIME NOT NULL,
+    PRIMARY KEY(`id`)
+);
