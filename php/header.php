047bcf160d8666ed251c57cf6e60af7059a63390
Tommaso Guidi <S0R3d@users.noreply.github.com>
2023-01-04 13:22:05 +0100

    Added basic header and footer

---
+++ php/header.php
@@ -0,0 +1,18 @@
+<header style="display: flex;">
+    <span>Header</span>
+    <div class="today-botton">
+        <button>
+            today
+        </button>
+    </div>
+    <div class="arrow-left">
+        <button>
+            < </button>
+    </div>
+    <div class="arrow-right">
+        <button>
+            >
+        </button>
+    </div>
+    <div class="month year">Gennaio 2023</div>
+</header>
