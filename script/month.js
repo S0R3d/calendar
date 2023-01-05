3a32293715e4331153532983be22402b6691adf4
Tommaso Guidi <S0R3d@users.noreply.github.com>
2023-01-05 19:02:13 +0100

    Remove unused line and added TODOs

--- script/month.js
+++ script/month.js
@@ -1,52 +1,39 @@
+const now = new Date();
 const months = [
   "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",
 ];
-const monthDays = [
-  31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
-];
+const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-const now = new Date();
-console.log("Today is: " + now.toString());
-
-// re-write toString method for array (there are only 2 element, number) to print month and year correctlly
 Array.prototype.toString = function () {
-  // run this for specific Array of number
   if (this.length == 2 && typeof this[0] == "number" && typeof this[1] == "number")
     return months[this[0]] + " " + this[1];
-  // run this for otherelse Array
   else return this.join();
 }
-// can change based of corrent month and year in calendar
 let pageCurrentMonthYear = [now.getMonth(), now.getFullYear()];
-console.log("Page current date: " + pageCurrentMonthYear.toString());
 
 // insert month and year in header section
-const changeHeaderMonthYear = () => {
+function changeHeaderMonthYear() {
   const monthYear = document.querySelector("div.month.year");
   monthYear.innerHTML = `${pageCurrentMonthYear}`;
 }
 changeHeaderMonthYear();
 
 // loading page with corrent month
-// TODO: create function to loding month
-const calendar = document.querySelector("div.calendar");
-const month = document.createElement("div");
-month.className = "month";
-month.style =
-  "display: grid; grid-template-columns: repeat(7,auto); text-align: center;";
-for (let i = 1; i < monthDays[pageCurrentMonthYear[0]] + 1; ++i) {
-  const day = document.createElement("div");
-  day.className = i % 7 == 0 ? "day sunday" : "day";
-  day.innerHTML = `${i}`;
-  month.appendChild(day);
+function loadCalendar() {
+  const calendar = document.querySelector("div.calendar");
+  const month = document.createElement("div");
+  month.className = "month " + months[pageCurrentMonthYear[0]];
+  month.style = "display: grid; grid-template-columns: repeat(7,auto); text-align: center;";
+  for (let i = 1; i < monthDays[pageCurrentMonthYear[0]] + 1; ++i) {
+    const day = document.createElement("div");
+    day.className = i % 7 == 0 ? "day sunday" : "day";
+    day.innerHTML = `${i}`;
+    month.appendChild(day);
+  }
+  calendar.appendChild(month);
 }
-calendar.appendChild(month);
 
 // FIXME: rimuovere ridondanze 
-// TODO: aggiungere i cambiamenti sul calendario in base al mese selezionato 
-//        (FORSE NON SERVE) perchè rifaccio tutto in php
-// TODO: use js and ajax per eseguire script php per modificare/aggiungere gli eventi persenti del db all' interno
-//        del calendario
 const nextMonth = document.querySelector("div.right-arrow");
 if (nextMonth) {
   nextMonth.addEventListener("click", () => {
@@ -82,3 +69,13 @@ if (today) {
     changeHeaderMonthYear();
   })
 } else console.error("Not Found Button 'Today'");
+
+/**
+ * TODO: usare AJAX con jquery
+ * modificare il calendario cambiano i giorni in base al mese selezionato in 'pageCurrentMonthYear[0]'
+ * modificare gli eventi all'interno del calendario in base ai nuovi giorni e al nuovo mese
+ * scrivere script query per prendere gli eventi dal db
+ */
+$(document).ready(function () {
+
+});
