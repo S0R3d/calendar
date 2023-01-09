bf1c3cdcc12d5ba4b802f04d1e572d88f929747a
Tommaso Guidi <S0R3d@users.noreply.github.com>
2023-01-09 17:10:52 +0100

    Update with current table name

--- php/table.php
+++ php/table.php
@@ -1,12 +1,16 @@
 <?php
 require_once 'conn.php';
-$table = 'test';
+$table = 'events';
 try {
-    $db->exec('CREATE TABLE IF NOT EXISTS test(
-            id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-            name VARCHAR(255),
-            PRIMARY KEY(id)
-            )');
+    $db->exec('CREATE TABLE IF NOT EXISTS `events` (
+      `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+      `title` varchar(255) NOT NULL,
+      `sDate` date NOT NULL,
+      `sTime` time DEFAULT NULL,
+      `fDate` date NOT NULL,
+      `fTime` time DEFAULT NULL,
+      PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;');
 } catch (PDOException $th) {
     echo 'Error massage: '.$th;
     die();
