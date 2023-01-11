98e7e82d4d27227f07ffa8c3b9835193b3eb5891
Tommaso Guidi <S0R3d@users.noreply.github.com>
2023-01-11 19:54:11 +0100

    Changed 'date' manipulation and added ajax script

--- script/month.js
+++ script/month.js
@@ -16,33 +16,67 @@ const months = [
   "December",
 ];
 const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-/**
- *
- */
-Array.prototype.toString = function () {
-  if (
-    this.length == 2 &&
-    typeof this[0] == "number" &&
-    typeof this[1] == "number"
-  )
-    return months[this[1] - 1] + " " + this[0];
-  else return this.join();
+// parte dal mese e anno corrente si puo muovere in base alle freccie della pagina
+let pageCurrentDate = [now.getFullYear(), now.getMonth() + 1];
+// parte dal primo del mese corrente, si muove per lavorare
+const movingDate = {
+  arr: [...pageCurrentDate, 1],
+  next: function () {
+    if (
+      (this.arr[2] == 31 && [1, 3, 5, 7, 8, 10, 12].includes(this.arr[1])) ||
+      (this.arr[2] == 30 && [4, 6, 9, 11].includes(this.arr[1])) ||
+      (this.arr[2] == 28 && this.arr[1] == 2)
+    ) {
+      this.arr[2] = 1;
+      this.arr[1] += 1;
+      if (this.arr[1] > 12) {
+        this.arr[1] = 1;
+        this.arr[0] += 1;
+      }
+    } else this.arr[2] += 1;
+  },
+  prev: function () {
+    if (this.arr[2] == 01 && [1, 3, 5, 7, 8, 10, 12].includes(this.arr[1])) {
+      this.arr[2] = 31;
+      this.arr[1] -= 1;
+      if (this.arr[1] < 1) {
+        this.arr[1] = 12;
+        this.arr[0] -= 1;
+      }
+    } else if (this.arr[2] == 01 && [4, 6, 9, 11].includes(this.arr[1])) {
+      this.arr[2] = 30;
+      this.arr[1] -= 1;
+      if (this.arr[1] < 1) {
+        this.arr[1] = 12;
+        this.arr[0] -= 1;
+      }
+    } else if (this.arr[2] == 28 && this.arr[1] == 2) {
+      this.arr[2] = 28;
+      this.arr[1] -= 1;
+      if (this.arr[1] < 1) {
+        this.arr[1] = 12;
+        this.arr[0] -= 1;
+      }
+    } else this.arr[2] -= 1;
+  },
 };
-let pageCurrentMonthYear = [now.getFullYear(), now.getMonth() + 1];
 
-function changeHeaderMonthYear() {
+function resetCurrentDate() {
+  pageCurrentDate = [now.getFullYear(), now.getMonth() + 1];
+}
+function resetMovingDate() {
+  movingDate.arr = [...pageCurrentDate, 1];
+}
+
+function changeHeader() {
   const monthYear = document.querySelector("div.header-month.header-year");
-  monthYear.innerHTML = `${pageCurrentMonthYear}`;
+  monthYear.innerHTML =
+    `${months[pageCurrentDate[1] - 1]}` + " " + `${pageCurrentDate[0]}`;
 }
-changeHeaderMonthYear();
-
-function resetCurrent() {
-  pageCurrentMonthYear = [now.getFullYear(), now.getMonth() + 1];
-}
+changeHeader();
 
 // TODO: eseguire questa funzione insieme agli script php per gli eventi
-function dayNameGenerator() {
+function daysGenerator() {
   const days = document.querySelectorAll("div.day");
   days.forEach((day) => {
     const dayName = day.id;
@@ -50,57 +84,8 @@ function dayNameGenerator() {
     if (inner == "") day.firstElementChild.children[0].innerHTML = dayName;
   });
 
-  const format = {
-    arr: [...pageCurrentMonthYear, 1],
-
-    /**
-     * Move to the next day of the month
-     */
-    next: function () {
-      if (
-        (this.arr[2] == 31 && [1, 3, 5, 7, 8, 10, 12].includes(this.arr[1])) ||
-        (this.arr[2] == 30 && [4, 6, 9, 11].includes(this.arr[1])) ||
-        (this.arr[2] == 28 && this.arr[1] == 2)
-      ) {
-        this.arr[2] = 1;
-        this.arr[1] += 1;
-        if (this.arr[1] > 12) {
-          this.arr[1] = 1;
-          this.arr[0] += 1;
-        }
-      } else this.arr[2] += 1;
-    },
-    /**
-     * Move to the previous day of the month
-     */
-    prev: function () {
-      if (this.arr[2] == 01 && [1, 3, 5, 7, 8, 10, 12].includes(this.arr[1])) {
-        this.arr[2] = 31;
-        this.arr[1] -= 1;
-        if (this.arr[1] < 1) {
-          this.arr[1] = 12;
-          this.arr[0] -= 1;
-        }
-      } else if (this.arr[2] == 01 && [4, 6, 9, 11].includes(this.arr[1])) {
-        this.arr[2] = 30;
-        this.arr[1] -= 1;
-        if (this.arr[1] < 1) {
-          this.arr[1] = 12;
-          this.arr[0] -= 1;
-        }
-      } else if (this.arr[2] == 28 && this.arr[1] == 2) {
-        this.arr[2] = 28;
-        this.arr[1] -= 1;
-        if (this.arr[1] < 1) {
-          this.arr[1] = 12;
-          this.arr[0] -= 1;
-        }
-      } else this.arr[2] -= 1;
-    },
-  };
-
+  const format = movingDate;
   let isCurrentMonth = true;
-
   let thisDay = new Date(format.arr);
   while (thisDay.getDay() != 1) {
     isCurrentMonth = false;
@@ -108,7 +93,6 @@ function dayNameGenerator() {
     thisDay = new Date(format.arr);
   }
 
-  // FIXME: -BUG- i varoli all'interno di PageCurrentMonthYear non vengono resettati a quelli corretti
   days.forEach((day) => {
     let numberOfWeek = thisDay.getDay();
     let dayNumb = thisDay.getDate();
@@ -121,19 +105,16 @@ function dayNameGenerator() {
       } else day.firstElementChild.children[1].innerHTML = dayNumb;
       format.next();
       thisDay = new Date(format.arr);
-      if (thisDay.getMonth() == pageCurrentMonthYear[1] - 1)
-        isCurrentMonth = true;
+      if (thisDay.getMonth() == pageCurrentDate[1] - 1) isCurrentMonth = true;
       else isCurrentMonth = false;
     }
   });
-  resetCurrent();
+  resetCurrentDate();
+  resetMovingDate();
 }
 
-/**
- * This function running on 'onLoad' attribute inside body HTML tag
- */
 function bodyOnLoad() {
-  dayNameGenerator();
+  daysGenerator();
 }
 
 // FIXME: rimuovere ridondanze
@@ -141,63 +122,67 @@ function bodyOnLoad() {
 const nextMonth = document.querySelector("div.right-arrow");
 if (nextMonth) {
   nextMonth.addEventListener("click", () => {
-    if (pageCurrentMonthYear[1] == 12) {
-      pageCurrentMonthYear[1] = 1;
-      pageCurrentMonthYear[0] += 1;
+    if (pageCurrentDate[1] == 12) {
+      pageCurrentDate[1] = 1;
+      pageCurrentDate[0] += 1;
     } else {
-      pageCurrentMonthYear[1] += 1;
-      pageCurrentMonthYear[0];
+      pageCurrentDate[1] += 1;
+      pageCurrentDate[0];
     }
-    changeHeaderMonthYear();
+    changeHeader();
   });
 } else console.error("Not Found Button 'Next Month'!");
 
 const previousMonth = document.querySelector("div.left-arrow");
 if (previousMonth) {
   previousMonth.addEventListener("click", () => {
-    if (pageCurrentMonthYear[1] == 1) {
-      pageCurrentMonthYear[1] = 12;
-      pageCurrentMonthYear[0] -= 1;
+    if (pageCurrentDate[1] == 1) {
+      pageCurrentDate[1] = 12;
+      pageCurrentDate[0] -= 1;
     } else {
-      pageCurrentMonthYear[1] -= 1;
-      pageCurrentMonthYear[0];
+      pageCurrentDate[1] -= 1;
+      pageCurrentDate[0];
     }
-    changeHeaderMonthYear();
+    changeHeader();
   });
 } else console.error("Not Found Button 'Previous Month'!");
 
 const today = document.querySelector("div.today-button");
 if (today) {
   today.addEventListener("click", () => {
-    pageCurrentMonthYear = [now.getFullYear(), now.getMonth() + 1];
-    changeHeaderMonthYear();
+    resetCurrentDate();
+    changeHeader();
   });
 } else console.error("Not Found Button 'Today'");
 
 /**
  * TODO: usare AJAX con jquery
- * modificare il calendario cambiano i giorni in base al mese selezionato in 'pageCurrentMonthYear[0]'
+ * modificare il calendario cambiano i giorni in base al mese selezionato in 'pageCurrentDate[0]'
  * modificare gli eventi all'interno del calendario in base ai nuovi giorni e al nuovo mese
  * scrivere script query per prendere gli eventi dal db
  */
 $(document).ready(function () {
-  console.log("ajax running...");
   const days = Object.values(document.querySelectorAll("div.day"));
-  console.log(days);
   days.forEach((day) => {
     if (!day.classList.contains("transparency")) {
+      let d = movingDate.arr[2];
+      let m = movingDate.arr[1];
+      let y = movingDate.arr[0];
       $.ajax({
         type: "POST",
         url: "php/loadData.php",
         data: {
-          day: 1,
-          month: pageCurrentMonthYear[1],
-          year: pageCurrentMonthYear[0],
+          day: d,
+          month: m,
+          year: y,
         },
         success: function (response) {
           day.innerHTML += response;
         },
       });
+      movingDate.next();
     }
   });
+  resetCurrentDate();
+  resetMovingDate();
 });
