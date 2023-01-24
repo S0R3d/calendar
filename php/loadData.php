a22baa0153ac4fd03ebdbd1bdab96aec4892b710
Tommaso Guidi <S0R3d@users.noreply.github.com>
2023-01-24 20:48:31 +0100

    UPDATE: data loading from db

--- php/loadData.php
+++ php/loadData.php
@@ -5,20 +5,19 @@ $query = "SELECT * FROM `events` WHERE `sDate` = :start ";
 $state = $db->prepare($query);
 $state->execute(['start' => $start]);
 $data = $state->fetchAll();
-// var_dump($data);
 if (sizeof($data)) {
     // limit 3: 3 eventi vanno inseriti ora
-    for ($i = 0; $i < count($data); $i++) {
-        $sD = $data[$i]['sDate'];
-        $fD = $data[$i]['fDate'];
-        $sT = substr($data[$i]['sTime'], 0, strlen($data[$i]['sTime']) - 3);
-        $fT = substr($data[$i]['fTime'], 0, strlen($data[$i]['fTime']) - 3);
+    foreach($data as $i => $row) {
+        $sD = $row['sDate'];
+        $fD = $row['fDate'];
+        $sT = substr($row['sTime'], 0, strlen($row['sTime']) - 3);
+        $fT = substr($row['fTime'], 0, strlen($row['fTime']) - 3);
 
         if ($sD == $fD and ($sT == $fT and $fT == "00:00")) { // Fgg ?>
             <div class="event Fgg">
                 <div class="icon"></div>
                 <div class="title">
-                    <?php echo $data[$i]['title'] ?>
+                    <?php echo $row['title'] ?>
                 </div>
             </div>
         <?php
@@ -28,7 +27,7 @@ if (sizeof($data)) {
             <div class="event Fggs">
                 <div class="icon"></div>
                 <div class="title">
-                    <?php echo $data[$i]['title'] ?>
+                    <?php echo $row['title'] ?>
                 </div>
             </div>
         <?php
@@ -44,20 +43,29 @@ if (sizeof($data)) {
                 <div class="icon"></div>
                 <div class="title">
                     <?php echo $sT ?>
-                    <?php echo $data[$i]['title'] ?>
+                    <?php echo $row['title'] ?>
                 </div>
             </div>
         <?php
         } else if ($sD != $fD and $sT != $fT) { // NFggs 
-        // FIXME: i giorni multipli NON funzionano, carica solo nel primo giorni(sDate) ?>
+        // FIXME: i giorni multipli NON funzionano, carica solo nel primo giorni(sDate)
+            $diff = (substr($fD,8) - substr($sD,8));
+        ?>
             <div class="event NFggs">
                 <div class="icon"></div>
                 <div class="title">
                     <?php echo $sT ?>
-                    <?php echo $data[$i]['title'] ?>
+                    <?php echo $row['title'] ?>
                 </div>
             </div>
         <?php
+            for ($i=0; $i < $diff; $i++) { ?>
+                <div class="event NFggs end-evt">
+                    <div class="icon"></div>
+                    <div class="title"></div>
+                </div>
+            <?php
+            }
         }
     }
 } ?>
