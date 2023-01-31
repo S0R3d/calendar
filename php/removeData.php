6622887b3dfb3fa7a924799f8c51abfc7af95380
Tommaso Guidi <S0R3d@users.noreply.github.com>
2023-01-31 19:02:34 +0100

    UPDATE: add query to delete events

--- php/removeData.php
+++ php/removeData.php
@@ -2,11 +2,8 @@
 require_once("conn.php");
 $date = $_POST['y']."-".$_POST['m']."-".$_POST['d'];
 $id = $_POST['id'];
-
-echo $date;
-echo " ";
-echo $id;
-
 $query = "DELETE FROM `events` WHERE `id` = :id";
-
+$state = $db->prepare($query);
+$state->execute(['id' => $id]);
+$res = $state->fetchAll();
 ?>
