5bd137bd8afe3e6cb7ec85f844deaca469b0afef
Tommaso Guidi <S0R3d@users.noreply.github.com>
2023-02-07 18:49:36 +0100

    UPDATE: query for new SQL schema

--- php/removeData.php
+++ php/removeData.php
@@ -2,7 +2,8 @@
 require_once("conn.php");
 $date = $_POST['y']."-".$_POST['m']."-".$_POST['d'];
 $id = $_POST['id'];
-$query = "DELETE FROM `events` WHERE `id` = :id";
+$query = "DELETE FROM `event` WHERE `real_evt_id` = :id;
+            DELETE FROM `event` WHERE `id` = :id;";
 $state = $db->prepare($query);
 $state->execute(['id' => $id]);
 $res = $state->fetchAll();
