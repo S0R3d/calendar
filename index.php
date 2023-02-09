e4164cead45a173812a5bbf6a3cf6c57c1652e2f
Tommaso Guidi <S0R3d@users.noreply.github.com>
2023-02-09 15:16:44 +0100

    ADDED: favicon

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ $months = array(
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/calendar.svg" type="image/x-icon">
     <title>Tommi's Calendar</title>
     <link rel="stylesheet" href="style/main.css">
 </head>
